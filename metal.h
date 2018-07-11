fe0861b8812db07c89b7acfb7a363aab5a0d29c2
Stewart Dryden Smith <stewart.dryden.smith@gmail.com>
2018-07-10 22:05:59 -0700

    Add fuzziness to metal.

--- metal.h
+++ metal.h
@@ -6,15 +6,16 @@
 class Metal : public Material
 {
 public:
-    Metal(const Vec3& albedo) : albedo(albedo) {}
+    Metal(const Vec3& albedo, float fuzz) : albedo(albedo), fuzz(fuzz < 1.0f ? fuzz : 1.0f) {}
 
     virtual bool scatter(const Ray& r_in, const Hit_Record& rec, Vec3& attenuation, Ray& scattered) const {
         Vec3 reflected = reflect(unit_vector(r_in.direction()), rec.normal);
-        scattered = Ray(rec.p, reflected);
+        scattered = Ray(rec.p, reflected + fuzz * rand_in_unit_sphere());
         attenuation = albedo;
         return dot(scattered.direction(), rec.normal) > 0;
     }
 
     Vec3 albedo;
+    float fuzz;
 };
 
