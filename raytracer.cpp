ff5f71deb1e04a7a1719d4fac4dbc1202db70513
Stewart Smith <stewart.dryden.smith@gmail.com>
2018-09-14 05:00:27 -0700

    Adds easy config and includes config into in image file.

--- raytracer.cpp
+++ raytracer.cpp
@@ -2,6 +2,7 @@
 #include "hitable.h"
 #include "hitable_list.h"
 #include "ray.h"
+#include "strutil.h"
 #include "utility.h"
 #include "vec3.h"
 
@@ -15,41 +16,69 @@
 
 #ifdef _MSC_VER
 // Ignore warnings for unsafe std::gmtime
-#pragma warning(disable:4996)
+#pragma warning(disable : 4996)
 #endif
 
 #include <cassert>
 #include <chrono>
 #include <cstdio>
 #include <cstdlib>
-#include <ctime>   // std::gmtime
+#include <ctime> // std::gmtime
+#include <fstream>
 #include <iomanip> // std::put_time
 #include <limits>
 #include <memory>
-#include <fstream>
+#include <sstream>
 #include <vector>
 
 struct Viewport {
     int width;  /** Pixel width */
     int height; /** Pixel height */
 
-    float aspect_ratio() const {
-        return width / static_cast<float>(height);
-    }
+    float aspect_ratio() const { return width / static_cast<float>(height); }
 };
 
-Vec3 color(const Ray& r, const Hitable* world, int depth) {
+struct Config {
+    unsigned int rand_seed; // seed for randomization
+    Viewport viewport;
+    Ray eye;
+    Vec3 up;
+    float vert_fov; // vertical field of view (in degrees)
+    float aperture;
+    int aa_sample_size; // anti-aliasing sample size per pixel
+    int max_ray_depth;
+};
+
+std::string to_file_format(const char* ln_prefix, const Config& cfg)
+{
+    return Str_Util::format(
+        "%s Seed            : %X\n"
+        "%s Window          : %dx%d\n"
+        "%s Camera          : origin(%s), fwd(%s), up(%s)\n"
+        "%s FOV (vert)      : %f\n"
+        "%s Aperture        : %f\n"
+        "%s AA sample/px    : %d\n"
+        "%s Ray depth (max) : %d\n",
+        ln_prefix, cfg.rand_seed, ln_prefix, cfg.viewport.width,
+        cfg.viewport.height, ln_prefix, cfg.eye.origin().to_string().c_str(),
+        cfg.eye.direction().to_string().c_str(), cfg.up.to_string().c_str(),
+        ln_prefix, cfg.vert_fov, ln_prefix, cfg.aperture, ln_prefix,
+        cfg.aa_sample_size, ln_prefix, cfg.max_ray_depth);
+}
+
+Vec3 color(const Ray& r, const Hitable* world, int depth, int max_depth)
+{
     Hit_Record rec;
 
-    constexpr int max_depth = 50;
-
-    if (world->hit(r, 0.0f + flop_err_thresh, std::numeric_limits<float>::max(), rec)) {
+    if (world->hit(r, 0.0f + flop_err_thresh, std::numeric_limits<float>::max(),
+                   rec)) {
         assert(rec.material);
 
         Ray scattered;
         Vec3 attenuation;
-        if (depth < max_depth && rec.material->scatter(r, rec, attenuation, scattered)) {
-            return attenuation * color(scattered, world, depth + 1);
+        if (depth < max_depth &&
+            rec.material->scatter(r, rec, attenuation, scattered)) {
+            return attenuation * color(scattered, world, depth + 1, max_depth);
         } else {
             return Vec3(0.0f, 0.0f, 0.0f);
         }
@@ -61,43 +90,54 @@ Vec3 color(const Ray& r, const Hitable* world, int depth) {
     }
 }
 
-void antialias(const Viewport& viewport, const Camera& cam, const Hitable* world, int pix_u, int pix_v, Vec3& col) {
-    constexpr int sample_size = 100;
-    
-    for (int s = 0; s < sample_size; ++s) {
-        float u = static_cast<float>(pix_u + rand_normalized()) / static_cast<float>(viewport.width);
-        float v = static_cast<float>(pix_v + rand_normalized()) / static_cast<float>(viewport.height);
+void antialias(const Config& cfg, const Camera& cam, const Hitable* world,
+               int pix_u, int pix_v, Vec3& col)
+{
+    for (int s = 0; s < cfg.aa_sample_size; ++s) {
+        float u = static_cast<float>(pix_u + rand_normalized()) /
+                  static_cast<float>(cfg.viewport.width);
+        float v = static_cast<float>(pix_v + rand_normalized()) /
+                  static_cast<float>(cfg.viewport.height);
         Ray r = cam.to_viewport(u, v);
         Vec3 p = r.point(2.0f); // TODO: Should this be a constant?
-        col += color(r, world, 0);
+        col += color(r, world, 0, cfg.max_ray_depth);
     }
 
-    col /= static_cast<float>(sample_size);
+    col /= static_cast<float>(cfg.aa_sample_size);
 }
 
-Vec3 gamma_correction(const Vec3& col) {
+Vec3 gamma_correction(const Vec3& col)
+{
     // Use Gamma 2 (1/gamma => 1/2)
 
     return Vec3(sqrt(col.r()), sqrt(col.g()), sqrt(col.b()));
 }
 
-void gen_ppm(const Viewport& viewport, const Camera& cam, const Hitable* world)
+void gen_ppm(const Config& cfg, const Hitable* world)
 {
+    const Viewport& viewport = cfg.viewport;
+    const float dist_to_focus =
+        (cfg.eye.origin() - cfg.eye.direction()).length();
+    Camera cam(cfg.eye, cfg.up, cfg.vert_fov, cfg.viewport.aspect_ratio(),
+               cfg.aperture, dist_to_focus);
+
     std::ofstream imageFile("image.ppm", std::ios::out | std::ios::trunc);
     if (!imageFile.is_open()) {
         printf("Unable to open output file\n");
         return;
     }
 
-    imageFile << "P3\n" << viewport.width << " " << viewport.height << "\n255\n";
+    imageFile << to_file_format("#", cfg);
+    imageFile << "P3\n"
+              << viewport.width << " " << viewport.height << "\n255\n";
 
-    // Traverse from the viewport's lower left corner to top right corner, 
+    // Traverse from the viewport's lower left corner to top right corner,
     // left to right, bottom to top.
     for (int j = viewport.height - 1; j >= 0; --j) {
         for (int i = 0; i < viewport.width; ++i) {
             Vec3 col(0.0f, 0.0f, 0.0f);
 
-            antialias(viewport, cam, world, i, j, col);
+            antialias(cfg, cam, world, i, j, col);
             assert(!is_nan(col));
 
             col = gamma_correction(col);
@@ -114,7 +154,8 @@ void gen_ppm(const Viewport& viewport, const Camera& cam, const Hitable* world)
     imageFile.close();
 }
 
-std::vector<Hitable*> random_scene() {
+std::vector<Hitable*> random_scene()
+{
     std::vector<Hitable*> list;
 
     list.emplace_back(
@@ -123,7 +164,8 @@ std::vector<Hitable*> random_scene() {
     for (int a = -11; a < 11; ++a) {
         for (int b = -11; b < 11; ++b) {
             const float choose_mat = rand_normalized();
-            Vec3 center(a + 0.9f * rand_normalized(), 0.2f, b + 0.9f * rand_normalized());
+            Vec3 center(a + 0.9f * rand_normalized(), 0.2f,
+                        b + 0.9f * rand_normalized());
             if ((center - Vec3(4.0f, 0.2f, 0.0f)).length() > 0.9f) {
                 if (choose_mat < 0.8f) {
                     // diffuse
@@ -150,11 +192,12 @@ std::vector<Hitable*> random_scene() {
         }
     }
 
-    list.emplace_back(new Sphere({0.0f, 1.0f, 0.0f}, 1.0f, new Dielectric(1.5f)));
     list.emplace_back(
-        new Sphere({-4.0f, 1.0f, 0.0f}, 1.0f, new Lambertian({0.4f, 0.2f, 0.1f})));
-    list.emplace_back(
-        new Sphere({4.0f, 1.0f, 0.0f}, 1.0f, new Metal({0.7f, 0.6f, 0.5f}, 0.0f)));
+        new Sphere({0.0f, 1.0f, 0.0f}, 1.0f, new Dielectric(1.5f)));
+    list.emplace_back(new Sphere({-4.0f, 1.0f, 0.0f}, 1.0f,
+                                 new Lambertian({0.4f, 0.2f, 0.1f})));
+    list.emplace_back(new Sphere({4.0f, 1.0f, 0.0f}, 1.0f,
+                                 new Metal({0.7f, 0.6f, 0.5f}, 0.0f)));
 
     return list;
 }
@@ -163,10 +206,9 @@ std::vector<Hitable*> test_scene()
 {
     std::vector<Hitable*> list;
 
-    list.emplace_back(
-        new Sphere({0.0f, -100.5f, -1.0f}, 100.0f,
-                   new Lambertian({0.8f, 0.8f, 0.0f}))); // base
-    {   
+    list.emplace_back(new Sphere({0.0f, -100.5f, -1.0f}, 100.0f,
+                                 new Lambertian({0.8f, 0.8f, 0.0f}))); // base
+    {
         // glass sphere
         list.emplace_back(
             new Sphere({-1.0f, 0.0f, -1.0f}, 0.5f, new Dielectric(1.5f)));
@@ -181,27 +223,13 @@ std::vector<Hitable*> test_scene()
     return list;
 }
 
-struct Config {
-    unsigned int rand_seed; // seed for randomization
-    Viewport viewport;
-    Ray eye;
-    Vec3 up;
-    float vert_fov;      // vertical field of view (in degrees)
-    float aperture;
-    int aa_sample_size;  // anti-aliasing sample size per pixel
-    int max_ray_depth;
-};
-
-void raytrace(Config& cfg) {
+void raytrace(const Config& cfg)
+{
     srand(cfg.rand_seed);
 
     std::vector<Hitable*> hitables = test_scene(); // random_scene();
     std::unique_ptr<Hitable_List> world =
         std::make_unique<Hitable_List>(hitables.data(), hitables.size());
-
-    const float dist_to_focus = (cfg.eye.origin() - cfg.eye.direction()).length();
-    Camera cam(cfg.eye, cfg.up, cfg.vert_fov, cfg.viewport.aspect_ratio(),
-               cfg.aperture, dist_to_focus);
 
     auto time_point_start = std::chrono::system_clock::now();
     std::time_t t_start =
@@ -209,7 +237,7 @@ void raytrace(Config& cfg) {
     std::cout << "Start time: "
               << std::put_time(std::gmtime(&t_start), "%F %T UTC") << std::endl;
     {
-        gen_ppm(cfg.viewport, cam, world.get());
+        gen_ppm(cfg, world.get());
     }
     auto time_point_end = std::chrono::system_clock::now();
     std::time_t t_end = std::chrono::system_clock::to_time_t(time_point_end);
@@ -218,7 +246,8 @@ void raytrace(Config& cfg) {
 
     std::cout << "Duration  : "
               << std::chrono::duration_cast<std::chrono::seconds>(
-                     time_point_end - time_point_start).count()
+                     time_point_end - time_point_start)
+                     .count()
               << "s" << std::endl;
 
     // TODO: cleanup memory
@@ -226,16 +255,19 @@ void raytrace(Config& cfg) {
 
 int main()
 {
-    Config cfg;
+    Config cfg = {};
     cfg.rand_seed = 0;
-    cfg.viewport = {200, 100};
+    cfg.viewport = {1920, 1080}; //{200, 100};
     cfg.eye = {{3, 3, 2}, {0, 0, -1}};
     cfg.up = {0, 1, 0};
     cfg.vert_fov = 20;
     cfg.aperture = 2.0f;
+    cfg.aa_sample_size = 100;
+    cfg.max_ray_depth = 50;
+    //cfg.aa_sample_size = 1;
+    //cfg.max_ray_depth = 1;
 
     raytrace(cfg);
 
     return 0;
 }
-
--- vec3.h
+++ vec3.h
@@ -1,16 +1,19 @@
 #pragma once
 
+#include "strutil.h"
+
 #include <array>
 #include <cassert>
 #include <iostream>
 #include <math.h>
+#include <string>
 
 class Vec3
 {
 public:
     Vec3() : e{} {};
-    Vec3(float e0, float e1, float e2) : e{ e0, e1, e2 } {}
-    
+    Vec3(float e0, float e1, float e2) : e{e0, e1, e2} {}
+
     inline float x() const { return e[0]; }
     inline float y() const { return e[1]; }
     inline float z() const { return e[2]; }
@@ -33,6 +36,8 @@ public:
     inline float length() const;
     inline float squared_length() const;
     inline void make_unit_vector();
+
+    inline std::string to_string() const;
 
     std::array<float, 3> e;
 };
@@ -58,129 +63,154 @@ inline Vec3 unit_vector(const Vec3& v);
 
 ///////////////////////////////////////////////////////////////////////////////
 
-inline Vec3& Vec3::operator+=(const Vec3& o) {
+inline Vec3& Vec3::operator+=(const Vec3& o)
+{
     e[0] += o.e[0];
     e[1] += o.e[1];
     e[2] += o.e[2];
     return *this;
 }
 
-inline Vec3& Vec3::operator-=(const Vec3& o) {
+inline Vec3& Vec3::operator-=(const Vec3& o)
+{
     e[0] -= o.e[0];
     e[1] -= o.e[1];
     e[2] -= o.e[2];
     return *this;
 }
 
-inline Vec3& Vec3::operator*=(const Vec3& o) {
+inline Vec3& Vec3::operator*=(const Vec3& o)
+{
     e[0] *= o.e[0];
     e[1] *= o.e[1];
     e[2] *= o.e[2];
     return *this;
 }
-inline Vec3& Vec3::operator/=(const Vec3& o) {
+inline Vec3& Vec3::operator/=(const Vec3& o)
+{
     e[0] /= o.e[0];
     e[1] /= o.e[1];
     e[2] /= o.e[2];
     return *this;
 }
 
-inline Vec3& Vec3::operator*=(const float c) {
+inline Vec3& Vec3::operator*=(const float c)
+{
     e[0] *= c;
     e[1] *= c;
     e[2] *= c;
     return *this;
 }
-inline Vec3& Vec3::operator/=(const float c) {
+inline Vec3& Vec3::operator/=(const float c)
+{
     e[0] /= c;
     e[1] /= c;
     e[2] /= c;
     return *this;
 }
 
-inline float Vec3::length() const {
+inline float Vec3::length() const
+{
     return sqrt(squared_length());
 }
 
-inline float Vec3::squared_length() const {
+inline float Vec3::squared_length() const
+{
     return e[0] * e[0] + e[1] * e[1] + e[2] * e[2];
 }
 
-inline void Vec3::make_unit_vector() {
+inline void Vec3::make_unit_vector()
+{
     float k = 1.0f / squared_length();
     *this *= k;
 }
 
-inline std::istream& operator>>(std::istream& is, Vec3& v) {
+inline std::string Vec3::to_string() const
+{
+    return Str_Util::format("%f, %f, %f", e[0], e[1], e[2]);
+}
+
+inline std::istream& operator>>(std::istream& is, Vec3& v)
+{
     return is >> v.e[0] >> v.e[1] >> v.e[2];
 }
 
-inline std::ostream& operator<<(std::ostream& os, Vec3& v) {
+inline std::ostream& operator<<(std::ostream& os, Vec3& v)
+{
     return os << v.e[0] << " " << v.e[1] << " " << v.e[2];
 }
 
-inline bool operator==(const Vec3& lhs, const Vec3& rhs) {
-    return (lhs.e[0] == rhs.e[0]) 
-        && (lhs.e[1] == rhs.e[1]) 
-        && (lhs.e[2] == rhs.e[2]);
+inline bool operator==(const Vec3& lhs, const Vec3& rhs)
+{
+    return (lhs.e[0] == rhs.e[0]) && (lhs.e[1] == rhs.e[1]) &&
+           (lhs.e[2] == rhs.e[2]);
 }
 
-inline bool operator!=(const Vec3& lhs, const Vec3& rhs) {
+inline bool operator!=(const Vec3& lhs, const Vec3& rhs)
+{
     return !(lhs == rhs);
 }
 
-inline Vec3 operator+(const Vec3& v1, const Vec3& v2) {
+inline Vec3 operator+(const Vec3& v1, const Vec3& v2)
+{
     Vec3 u(v1);
     u += v2;
     return u;
 }
-inline Vec3 operator-(const Vec3& v1, const Vec3& v2) {
+inline Vec3 operator-(const Vec3& v1, const Vec3& v2)
+{
     Vec3 u(v1);
     u -= v2;
     return u;
 }
 
-inline Vec3 operator*(const Vec3& v1, const Vec3& v2) {
+inline Vec3 operator*(const Vec3& v1, const Vec3& v2)
+{
     Vec3 u(v1);
     u *= v2;
     return u;
 }
 
-inline Vec3 operator/(const Vec3& v1, const Vec3& v2) {
+inline Vec3 operator/(const Vec3& v1, const Vec3& v2)
+{
     Vec3 u(v1);
     u /= v2;
     return u;
 }
 
-inline Vec3 operator*(const Vec3& v, const float c) {
+inline Vec3 operator*(const Vec3& v, const float c)
+{
     Vec3 u(v);
     u *= c;
     return u;
 }
 
-inline Vec3 operator*(const float c, const Vec3& v) {
+inline Vec3 operator*(const float c, const Vec3& v)
+{
     return v * c;
 }
 
-inline Vec3 operator/(const Vec3& v, const float c) {
+inline Vec3 operator/(const Vec3& v, const float c)
+{
     Vec3 u(v);
     u /= c;
     return u;
 }
 
-inline float dot(const Vec3& v1, const Vec3& v2) {
+inline float dot(const Vec3& v1, const Vec3& v2)
+{
     Vec3 u = v1 * v2;
     return u.e[0] + u.e[1] + u.e[2];
 }
-inline Vec3 cross(const Vec3& v1, const Vec3& v2) {
-    return Vec3(
-        (v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
-        (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
-        (v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0])
-    );
+inline Vec3 cross(const Vec3& v1, const Vec3& v2)
+{
+    return Vec3((v1.e[1] * v2.e[2] - v1.e[2] * v2.e[1]),
+                (-(v1.e[0] * v2.e[2] - v1.e[2] * v2.e[0])),
+                (v1.e[0] * v2.e[1] - v1.e[1] * v2.e[0]));
 }
 
-inline Vec3 unit_vector(const Vec3& v) {
+inline Vec3 unit_vector(const Vec3& v)
+{
     assert(v.length() > 0);
     return v / v.length();
 }
