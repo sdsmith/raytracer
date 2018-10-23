66b6ce96c5b6acd71fabd2d2eb1ac29bede4e80a
Stewart Smith <stewart.dryden.smith@gmail.com>
2018-10-23 00:15:05 -0700

    Adds 32 and 64 bit build options. Cleans up makefile spew.

--- make_linux_builds.sh
+++ make_linux_builds.sh
@@ -3,7 +3,13 @@
 # Exit on error
 set -e
 
-make CXX=g++ BUILD=debug
-make CXX=g++ BUILD=release
-make CXX=clang++ BUILD=debug
-make CXX=clang++ BUILD=release
+make CXX=g++     BUILD=debug   ARCH=x64 $*
+make CXX=g++     BUILD=release ARCH=x64 $*
+make CXX=clang++ BUILD=debug   ARCH=x64 $*
+make CXX=clang++ BUILD=release ARCH=x64 $*
+
+## Can't build 32bit on cygwin
+# make CXX=g++     BUILD=debug   ARCH=x86 $*
+# make CXX=g++     BUILD=release ARCH=x86 $*
+# make CXX=clang++ BUILD=debug   ARCH=x86 $*
+# make CXX=clang++ BUILD=release ARCH=x86 $*
