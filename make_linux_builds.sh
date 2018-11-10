e5488a3a3802667ae39cacdec5fad0c815db5b6d
Stewart D. Smith <stewart.dryden.smith@gmail.com>
2018-11-10 12:17:10 -0800

    Renable 32-bit builds.

--- make_linux_builds.sh
+++ make_linux_builds.sh
@@ -8,8 +8,8 @@ make CXX=g++     BUILD=release ARCH=x64 $*
 make CXX=clang++ BUILD=debug   ARCH=x64 $*
 make CXX=clang++ BUILD=release ARCH=x64 $*
 
-## Can't build 32bit on cygwin
-# make CXX=g++     BUILD=debug   ARCH=x86 $*
-# make CXX=g++     BUILD=release ARCH=x86 $*
-# make CXX=clang++ BUILD=debug   ARCH=x86 $*
-# make CXX=clang++ BUILD=release ARCH=x86 $*
+# Can't build 32bit on cygwin
+make CXX=g++     BUILD=debug   ARCH=x86 $*
+make CXX=g++     BUILD=release ARCH=x86 $*
+make CXX=clang++ BUILD=debug   ARCH=x86 $*
+make CXX=clang++ BUILD=release ARCH=x86 $*
